ea253f3ef64e25814ed703a1e788ea475d6f8201
huykhoa11 <69473375+huykhoa11@users.noreply.github.com>
2022-04-12 03:11:41 +0900

    ajax destroy

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,6 +1,7 @@
 class CardsController < ApplicationController
   def show
   	@order_items = current_order.order_items
+  	# @order_items = OrderItem.all
   end
 
 end
--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,4 +1,7 @@
 class OrderItemsController < ApplicationController
+	before_action :set_order_item, only: [:update, :destroy]	#siêu quan trọng, 
+																#trước khi bấm update hay destroy thì chương trình sẽ chạy hàm set_order_item trước
+
 	def create
 		@order = current_order
 		@order_item = @order.order_items.new(order_params)
@@ -11,17 +14,30 @@ class OrderItemsController < ApplicationController
 	end
 
 	def update
-		@order_item = current_order.order_items.find(params[:id])
+		
 		@order_item.update(order_params)
-		@order_items = current_order.order_items
+		
+		respond_to do |format|
+			format.js
+		end
 	end
 
 	def destroy
-	  	@order_item = current_order.order_items.find(params[:id])
+	  	
 	  	@order_item.destroy
+	  	
+	  	respond_to do |format|
+			format.js
+		end
   	end
 
 	private
+	def set_order_item
+		@order_item = current_order.order_items.find(params[:id])
+		@order_items = current_order.order_items
+	end
+
+
 	def order_params
 		params.require(:order_item).permit(:product_id, :quantity)
 	end
