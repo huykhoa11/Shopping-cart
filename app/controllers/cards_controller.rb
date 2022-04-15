3e93342ae093c50ca4f56d2a622d4b34fb1fb6b5
huykhoa11 <69473375+huykhoa11@users.noreply.github.com>
2022-04-15 18:40:16 +0900

    Add export CSV function

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,6 +2,10 @@ class CardsController < ApplicationController
   def show
   	@order_items = current_order.order_items
   	# @order_items = OrderItem.all
+  	respond_to do |format|
+  		format.html
+  		format.csv {send_data @order_items.generate_csv, filename: "orders-#{Time.zone.now.strftime("%Y%m%d%S")}.csv"}
+  	end
   end
 
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,9 +3,23 @@ class OrderItem < ApplicationRecord
     belongs_to :product
 
 
-
     before_save :set_unit_price
     before_save :set_total
+
+
+    def self.csv_attributes
+        ["product_id", "unit_price", "quantity", "total", "created_at"]
+    end
+
+    def self.generate_csv
+        CSV.generate(headers: true) do |csv|
+            csv << csv_attributes
+            all.each do |order|
+                csv << csv_attributes.map{|attr| order.send(attr)}
+            end
+        end
+    end
+
 
     def unit_price
         if persisted?
